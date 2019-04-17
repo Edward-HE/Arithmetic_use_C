e49613c5805fc2599ed8f75036d6f2c0f42153ea
Edward-HE <Edward-HE@users.noreply.github.com>
2019-04-18 01:26:04 +0800

    +四则运算

--- schoolwork1/schoolwork1.c
+++ schoolwork1/schoolwork1.c
@@ -9,6 +9,10 @@ typedef struct users_info {
 	char pswd[20];
 } users_info;
 
+int flag_login = 0;
+users_info a, b;
+char login_name[15];
+
 void users_create()
 {
 	FILE* fp;
@@ -57,7 +61,7 @@ void users_register() {//注册
 	return;
 }
 
-void users_login() {//登录
+char users_login() {//登录
 	users_info a, b;
 
 	FILE* fp;
@@ -67,6 +71,7 @@ void users_login() {//登录
 	fread(&b, sizeof(users_info), 1, fp);
 	printf("请输入账号\n");
 	scanf("%s", &a.name);
+	strcpy(login_name, a.name);
 	while (1) {
 		if (strcmp(a.name, b.name) == 0)/*如果已经注册了*/
 			break;
@@ -83,44 +88,197 @@ void users_login() {//登录
 	printf("请输入密码\n");
 	scanf("%s", &a.pswd);
 	int i = 3;
-	while(1) {
+	while (1) {
 		if (strcmp(a.pswd, b.pswd) == 0) {
 			fclose(fp);
-			printf("登录成功\n");
-			return;
+			printf("登录成功！即将返回主界面\n");
+			flag_login = 1;
+			Sleep(2000);
+			return (login_name);
 		}
 		else {
-			printf("密码不正确，请重新输入，您还有%d次机会\n",i);
+			printf("密码不正确，请重新输入，您还有%d次机会\n", i);
 			i--;
 			scanf("%s", &a.pswd);
 		}
 		if (i == 0) {
 			printf("您已输错超过3次，即将返回主界面\n");
-			Sleep(1000);
+			Sleep(2000);
 			system("cls");
 			break;
 		}
-			
-	} 
-
-
+	}
 }
+
+
+void doExercise()
+{
+	int score = 0;		//练习得分，初始化为0
+	int i = 0;			//练习题数的计数器
+	printf("===================================\n");
+	printf("\n欢迎使用小学生四则运算练习软件\n");
+	printf("\t1、加法练习\t2、减法练习\n");
+	printf("\t3、乘法练习\t4、除法练习\n");
+	printf("\t5、综合练习\t0、退出系统\n");
+	printf("请输入你的选择（0--5）：\n");
+	printf("\n===================================\n");
+	int op1,num;
+	scanf("%d", &op1);
+	printf("输入本次训练题数： ");
+	scanf("%d", &num);
+	for (i = 1; i <= num; i++)
+	{
+		score = score + test(op1);		//间接递归调用test(n)
+	}
+	printf("本次练习%d道题，你做对了%d道\n", num,score);
+}
+
+/*
+	功能：进行一道题的测试过程
+*/
+int test(int n)
+{
+	int ranswer = 0;			//正确答案
+	int uanswer = 0;			//用户输入的答案
+	int t = 0;				//临时变量
+	char operation;			//运算类别
+	int num1 = 0;				//操作数1
+	int num2 = 0;				//操作数2
+
+	srand(time(NULL));		//初始化随机数种子
+	num1 = rand() % 10;			//取0—9之间的随机数
+	num2 = rand() % 10;
+
+	//进入程序功能，如果选择5就随机产生1-4所代表的每一种运算方式
+	if (n == 5)
+	{
+		n = rand() % 4 + 1;
+	}
+
+	switch (n)
+	{
+	case 1:
+		operation = '+';
+		break;
+
+	case 2:
+		operation = '-';
+		break;
+
+	case 3:
+		operation = '*';
+		break;
+
+	case 4:
+		operation = '/';
+		break;
+	}
+
+	//加入一条防止“不够减”的语句，当选择减法且num1小于num2时不够减
+	//不够减的时候，用临时变量交换两个操作数
+	if ((operation == '-') && (num1 < num2))
+	{
+		t = num1;
+		num1 = num2;
+		num2 = t;
+	}
+
+	//防止“除数为0”的程序漏洞
+	//当num2被随机取值到0时，就将num2强制置为1
+	//做除法时，将num1的值强制赋值给num1*num2，防止不能整除
+	if (operation == '/')
+	{
+		if (num2 == 0)
+		{
+			num2 = 1;
+		}
+		num1 = num1 * num2;			//防止num1不能被num2整除的语句
+	}
+
+	//输出测试题本身、提示用户输入
+	printf("%d%c%d= ", num1, operation, num2);
+	scanf("%d", &uanswer);
+
+	//程序计算正确结果
+	switch (operation)
+	{
+	case '+':
+		ranswer = num1 + num2;
+		break;
+
+	case '-':
+		ranswer = num1 - num2;
+		break;
+
+	case '*':
+		ranswer = num1 * num2;
+		break;
+
+	case '/':
+		ranswer = num1 / num2;
+		break;
+	}
+	//评判，做对返回1，做错返回0
+	if (uanswer == ranswer)
+	{
+		printf("做对了！\n");
+		return 1;
+	}
+	else
+	{
+		printf("做错了！\n");
+		return 0;
+	}
+}
+
 
 int main() {
 	users_create();
 beginning:
-	printf("四则运算练习系统\n");
-	printf("1.注册\n");
-	printf("2.登录\n");
-	printf("6.退出系统\n");
-	printf("-.请输入对应序号\n");
+	printf("=============================================\n");
+	printf("\n\t      四则运算练习系统\n\n");
+	printf("\t1.注	册");
+	printf("\t2.登	录\n\n");
+	printf("\t3.关于我们");
+	printf("\t4.退出系统\n\n");
+	printf("\t      请输入对应序号: \n");
+	printf("\n=============================================\n");
 	int option;
 	scanf("%d", &option);
 	switch (option) {
 
-	case 1:users_register(); goto beginning;
-	case 2:users_login(); goto beginning;
-	case 6:exit(0);
+	case 1:users_register(); system("cls"); goto beginning;
+	case 2:users_login(); system("cls"); 
+mainly:
+		printf("=============================================\n");
+		printf("\n\t      四则运算练习系统\n\n");
+		printf("\t       当前用户：%s\n\n", login_name);
+		printf("\t1.四则运算");
+		printf("\t2.错题集\n\n");
+		printf("\t3.退出登录");
+		printf("\t4.退出系统\n\n");
+		printf("\t      请输入对应序号: \n");
+		printf("\n=============================================\n");
+		int option1;
+		scanf("%d", &option1);
+		switch (option1) {
+		case 1:
+			system("cls");
+			doExercise();
+			system("pause");
+			system("cls");
+			goto mainly;
+		case 2:
+
+		case 3:
+			system("cls");
+			goto beginning;
+		case 4:
+			exit(0);
+		}
+	case 3:
+		
+	case 4:exit(0);
 	}
 	return 0;
 }
